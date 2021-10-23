867707fa7bac4d937cf9c8c1d1dc3fee626d6fb6
Jefferson Santos Araújo <71133431+jefersonar-cmd@users.noreply.github.com>
2021-10-22 21:17:17 -0300

    implementando trailer modal

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
-
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
-  });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+});
